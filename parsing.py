e655af40af09891e291013d7319d57c700be00ec
DaniilFilatov <32264466+DaniilFilatov@users.noreply.github.com>
2017-10-11 19:00:52 +0300

    parser from Daniil

--- parsing.py
+++ parsing.py
@@ -1,1 +1,7 @@
-import scrapy
+import requests
+from bs4 import BeautifulSoup
+r=requests.get('https://vk.com/mamontok')
+print(r.ok)
+print('1)', r.text)
+page = BeautifulSoup(r.text, 'html.parser')
+print('2)', page)
