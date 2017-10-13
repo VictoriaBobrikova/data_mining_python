49e58c682ad8f6659a855e8c19da615d9669e651
DaniilFilatov <32264466+DaniilFilatov@users.noreply.github.com>
2017-10-13 22:44:30 +0300

    hometown

--- parsing.py
+++ parsing.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-r=requests.get('https://vk.com/mamontok')
+r=requests.get('https://api.vk.com/method/users.get.xml?user_ids=mamontok&fields=home_town')
 print(r.ok)
 print('1)', r.text)
 page = BeautifulSoup(r.text, 'html.parser')
