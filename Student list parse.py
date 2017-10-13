aee07bc5efc25254475a25c2a5e16ff2ef98946b
DaniilFilatov <32264466+DaniilFilatov@users.noreply.github.com>
2017-10-14 01:11:58 +0300

    last

--- Student list parse.py
+++ Student list parse.py
@@ -6,7 +6,7 @@ ua = UserAgent()
 print(ua.chrome)
 header = {'User-Agent':str(ua.chrome)}
 print(header)
-url = "https://vk.com/search?c%5Bcity%5D=2&c%5Bcountry%5D=1&c%5Bname%5D=1&c%5Bper_page%5D=40&c%5Bphoto%5D=1&c%5Bsection%5D=people&c%5Buniversity%5D=56"
+url = "https://vk.com/search?c%5Bcity%5D=2&c%5Bcountry%5D=1&c%5Bname%5D=1&c%5Bper_page%5D=40&c%5Bphoto%5D=1&c%5Bsection%5D=people&c%5Buniversity%5D=0"
 r=requests.get(url, headers=header)
 print(r.ok)
 print('1)', r.text)
