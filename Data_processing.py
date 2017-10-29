6c9c2be32f48d4a8d48dea82a8de194832a1499b
Victoria Bobrikova <viculka3005@gmail.com>
2017-10-29 19:25:18 +0400

    filter_errors

--- Data_processing.py
+++ Data_processing.py
@@ -10,7 +10,14 @@ with open(path, "r", newline="") as file:
     reader = csv.reader(file)
     i=0
     for row in reader:
-        row[0]=i
-        users.append(row)
-        print(row)
-        i=i+1
+        if row[3] == "Error" or row[3] == '' or row[3] == "Mudak":
+            i=i
+        elif i==0:
+            users.append(row)
+            print(row)
+            i = i + 1
+        else:
+            row[0]=i
+            users.append(row)
+            print(row)
+            i=i+1
