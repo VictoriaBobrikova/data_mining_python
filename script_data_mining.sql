2a2b4c4eb0646501cf006909dcaddcc34b313dba
Victoria Bobrikova <viculka3005@gmail.com>
2017-12-06 21:40:34 +0400

    create_script

---
+++ script_data_mining.sql
@@ -0,0 +1,47 @@
+-- Model: New Model    Version: 1.0
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`student_sex`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`student_sex` (
+  `student_name` VARCHAR(45) NOT NULL,
+  `student_sex` VARCHAR(45) NULL,
+  PRIMARY KEY (`student_name`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`student`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`student` (
+  `vk_id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `home_town` VARCHAR(45) NULL,
+  `student_sex_student_name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`vk_id`, `student_sex_student_name`),
+  INDEX `fk_student_student_sex_idx` (`student_sex_student_name` ASC),
+  CONSTRAINT `fk_student_student_sex`
+    FOREIGN KEY (`student_sex_student_name`)
+    REFERENCES `mydb`.`student_sex` (`student_name`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
