b6c660a8857d3c7a4450d32f5a9a0720f80bd608
DaniilFilatov <32264466+DaniilFilatov@users.noreply.github.com>
2017-12-09 21:19:35 +0300

    Final

--- Project_Jup.py
+++ Project_Jup.py
@@ -2,12 +2,12 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import requests
 import time
-import csv
 import matplotlib.pyplot as plt
 import numpy as np
 from prettytable import PrettyTable
 import sys
 import argparse
+import sqlite3
 
 
 def putUni():
@@ -23,138 +23,133 @@ if __name__ == '__main__':
     put = putUni()
     namespace = put.parse_args() #для разбора командной строки, это наш параметр введённый
 
+con = sqlite3.connect('my.db')  # создали соединение с БД
+cur = con.cursor()  # курсор, по строкам будет ходить
 
-def user_parser (j, id):
-    url = "https://api.vk.com/method/users.get.xml?user_ids="+id[1:]+"&fields=home_town"
+
+def create_table_student():
+    cur.execute(
+        'CREATE TABLE IF NOT EXISTS student (vk_id INT NOT NULL, name VARCHAR(45) NOT NULL, home_town VARCHAR(45) NOT NULL, PRIMARY KEY (vk_id), CONSTRAINT fk_student_student_sex  FOREIGN KEY (name) REFERENCES student_sex (student_name) ON DELETE NO ACTION ON UPDATE NO ACTION)')
+
+
+def create_table_student_sex():
+    cur.execute(
+        'CREATE TABLE IF NOT EXISTS student_sex (student_name VARCHAR(45) NOT NULL, student_sex VARCHAR(45) NULL, PRIMARY KEY (student_name))')
+
+
+def data_entry(value):
+    cur.execute('INSERT INTO student (vk_id, name, home_town) VALUES(?, ?, ?)', value)
+    con.commit()
+
+
+def data_entry_sex(value):
+    cur.execute('INSERT or IGNORE INTO student_sex (student_name, student_sex) VALUES(?, ?)', value)
+    con.commit()
+
+
+create_table_student()
+create_table_student_sex()
+
+
+def user_parser(j, id, sex):
+    if sex == 1:
+        sex1 = 'female'
+    else:
+        sex1 = 'male'
+    url = "https://api.vk.com/method/users.get.xml?user_ids=" + id[1:] + "&fields=home_town"
     r = requests.get(url)
     page = BeautifulSoup(r.text, 'html.parser')
+    try:  # (number, url, id, home_town, name, sex)
+        answer = (str(j), id, page.uid.get_text(), page.home_town.get_text(), page.first_name.get_text(), sex1)
+    except Exception:
+        answer = (str(j), id, page.uid.get_text(), 'Error', 'Error', sex1)
     try:
-        answer = [{'number': str(j),'url': id, 'id': page.uid.get_text(), 'home town': page.home_town.get_text(), 'name': page.first_name.get_text()}]
-    except AttributeError:
-        answer = [{'number': str(j),'url': id, 'id': page.uid.get_text(), 'home town': 'Error', 'name': page.first_name.get_text()}]
-    try:
-        writer.writerows(answer)
-    except UnicodeEncodeError:
-        answer = [{'number': str(j), 'url': id, 'id': page.uid.get_text(), 'home town': 'Mudak', 'name': page.first_name.get_text()}]
-        writer.writerows(answer)
+        if answer[3] != 'Error':
+            if answer[3] != '':
+                data_entry((answer[2], answer[4], answer[3]))
+                data_entry_sex((answer[4], answer[5]))
+    except Exception:
+        answer = (str(j), id, page.uid.get_text(), 'Error', 'Error', sex1)
+        if answer[3] != 'Error':
+            if answer[3] != '':
+                data_entry((answer[2], answer[4], answer[3]))
+                data_entry_sex((answer[4], answer[5]))
     time.sleep(0.33)
 
-
 ua = UserAgent()
-#print(ua.chrome)
 header = {'User-Agent':str(ua.chrome)}
-#print(header)
 
 
-path = "Home_Town.csv"
 j=0
 bdate=17
 k = namespace.n
 
-answer = [{'number': 'dict','url': 'dict', 'id': 'dict', 'home town': 'dict'}]
-with open(path, 'w', newline='') as file:
-    writer = csv.DictWriter(file, fieldnames=answer[0])
-    writer.writeheader()
-    while bdate<=25:
-        sex=1
-        while sex<=2:
-            i=0
-            while i<=48:
-                if i==0:
-                    url = "https://vk.com/search?c%5Bage_from%5D="+str(bdate)+"&c%5Bage_to%5D="+str(bdate)+"&c%5Bcity%5D=2&c%5Bcountry%5D=1&c%5Bname%5D=1&c%5Bper_page%5D=40&c%5Bphoto%5D=1&c%5Bsection%5D=people&c%5Bsex%5D="+str(sex)+"&c%5Buniversity%5D="+str(k)+"&offset=0"
-                else:
-                    url = "https://vk.com/search?c%5Bage_from%5D="+str(bdate)+"&c%5Bage_to%5D="+str(bdate)+"&c%5Bcity%5D=2&c%5Bcountry%5D=1&c%5Bname%5D=1&c%5Bper_page%5D=40&c%5Bphoto%5D=1&c%5Bsection%5D=people&c%5Bsex%5D="+str(sex)+"&c%5Buniversity%5D="+str(k)+"&offset="+str(20+i*20)
-                r=requests.get(url, headers=header)
-                print(r.ok)
-                page = BeautifulSoup(r.text, 'html.parser')
-                tags=page.findAll("div", {"class":"labeled name"})
-                #print (tags)
-                print('Page', i+1)
-                for tag in tags:
-                    some=tag.findAll("a", {"class": None})
-                    for link in some:
-                        j=j+1
-                        print (j,')' ,link.get('href'))
-                        user_parser(j, link.get('href'))
-                i=i+1
-                time.sleep(0.33)
-            sex=sex+1
-        bdate=bdate+1
-
-
-path = "Home_Town.csv"
-users = []
-cities = []
-with open(path, "r", newline="") as file:
-    reader = csv.reader(file)
-    i=0
-    for row in reader:
-        if row[3] == "Error" or row[3] == '' or row[3] == "Mudak":
-            i=i
-        elif i==0:
-            users.append(row)
-            #print(row)
-            i = i + 1
-        else:
-            row[0]=i
-            users.append(row)
-            #print(row)
+while bdate<=25:
+    sex=1
+    while sex<=2:
+        i=1
+        while i<=48:
+            if i==0:
+                url = "https://vk.com/search?c%5Bage_from%5D="+str(bdate)+"&c%5Bage_to%5D="+str(bdate)+"&c%5Bcity%5D=2&c%5Bcountry%5D=1&c%5Bname%5D=1&c%5Bper_page%5D=40&c%5Bphoto%5D=1&c%5Bsection%5D=people&c%5Bsex%5D="+str(sex)+"&c%5Buniversity%5D="+str(k)+"&offset=0"
+            else:
+                url = "https://vk.com/search?c%5Bage_from%5D="+str(bdate)+"&c%5Bage_to%5D="+str(bdate)+"&c%5Bcity%5D=2&c%5Bcountry%5D=1&c%5Bname%5D=1&c%5Bper_page%5D=40&c%5Bphoto%5D=1&c%5Bsection%5D=people&c%5Bsex%5D="+str(sex)+"&c%5Buniversity%5D="+str(k)+"&offset="+str(20+i*20)
+            r=requests.get(url, headers=header)
+            print(r.ok)
+            page = BeautifulSoup(r.text, 'html.parser')
+            tags=page.findAll("div", {"class":"labeled name"})
+            print('Page', i+1)
+            for tag in tags:
+                some=tag.findAll("a", {"class": None})
+                for link in some:
+                    j=j+1
+                    print (j,')' ,link.get('href'))
+                    user_parser(j, link.get('href'), sex)
             i=i+1
+            time.sleep(0.33)
+        sex=sex+1
+    bdate=bdate+1
 
-Num_people=len(users)
-
-for j in range(len(users)):
-    if          users[j][3] == 'Санкт - Петербург' or \
-                users[j][3] == 'Санкт Петербург' or\
-                users[j][3] == 'СПб' or\
-                users[j][3] == 'Питер' or\
-                users[j][3] == 'Saint-Petersburg' or \
-                users[j][3] == 'Петербург' or \
-                users[j][3] == 'Санкт-петербург' or \
-                users[j][3] == 'Санкт- Петербург' or \
-                users[j][3] == 'Санкт-Петербург ' or\
-                users[j][3] == 'санкт-петербург':
-        cities.append('Санкт-Петербург')
-    else:
-        cities.append(users[j][3])
-c=list(set(cities))
-
-for i in range(len(c)):  #считаем города, которые встречаются один раз, мусором и статистически не важными. Удаляем мусор
-    if cities.count(c[i])==1:
-        cities.remove(c[i])
-c=list(set(cities))
-
-c1=[]
-calc=[]
-for i in range(len(c)):
-    c1.append(cities.count(c[i]))
-for i in range(len(c)):
-    calc.append([c1[i],c[i]])
-calc.sort(reverse=True)
-print(calc)
+cur.execute(
+    'UPDATE student SET home_town = "Санкт-Петербург" WHERE home_town = "Санкт - Петербург" OR home_town = "Санкт Петербург" OR home_town = "СПб" OR home_town = "Питер" OR home_town = "Saint-Petersburg" OR home_town = "Петербург" OR home_town = "Санкт-петербург" OR home_town = "Санкт- Петербург" OR home_town = "Санкт-Петербург " OR home_town = "санкт-петербург"  OR home_town = "спб" OR home_town = "СПБ"')
+con.commit()
 
 
-city=['Город']
-city_num=['Число студентов, чел']
-city_percent=['В процентном соотношении, %']
-for i in range(len(calc)):
-    city_num.append(calc[i][0])
-    city_percent.append(calc[i][0]*100/Num_people)
-    city.append(calc[i][1])
-print('Число анализируемых пользователей =', Num_people)
-th=['N' ,city[0], city_num[0], city_percent[0]]
+cur.execute('SELECT COUNT(1) FROM student')
+str_vsego = cur.fetchall()
+vsego = str_vsego.pop(0)[0]
+print('Число анализируемых пользователей =', vsego)
+
+###получили списки для осей
+cur.execute('SELECT home_town, COUNT(home_town) FROM student GROUP BY home_town ORDER BY COUNT(home_town) DESC')
+data_town = cur.fetchall()
+cur.execute('SELECT home_town, COUNT(home_town) FROM student GROUP BY home_town ORDER BY COUNT(home_town) DESC')
+data_value = cur.fetchall()
+cur.execute('SELECT home_town, COUNT(home_town) FROM student GROUP BY home_town ORDER BY COUNT(home_town) DESC')
+data_num = cur.fetchall()
+
+towns = []
+values = []
+num_people = []
+values_percent = []
+b = 0
+while b < len(data_town):
+    towns.append(data_town.pop(b)[0])
+    num_people.append(data_num.pop(b)[1])
+    values.append(data_value.pop(b)[1])
+    values_percent.append(round(values.pop(b) * 100 / vsego, 2))
+
+th = ['N', 'Город', 'Число студентов, чел', 'В процентном соотношении, %']
 table = PrettyTable(th)
 for i in range(25):
-    i=i+1
-    table.add_row([i ,city[i], city_num[i], round(city_percent[i], 2)])
+    i = i + 1
+    table.add_row([i, towns[i - 1], num_people[i - 1], values_percent[i - 1]])
 print(table)
 
-
 fig = plt.figure()
-axes = fig.add_subplot (1, 1, 1)
-plt.barh(city[2:11], city_percent[2:11], align='center', color='orange')
-axes.set_xscale ('linear')
+axes = fig.add_subplot(1, 1, 1)
+plt.barh(towns[0:4], values_percent[0:4], align='center', color='orange')
+axes.set_xscale('linear')
 plt.title('Распределение студентов в %')
-plt.grid(True)   # линии вспомогательной сетки
+plt.grid(True)  # линии вспомогательной сетки
 plt.show()
 
